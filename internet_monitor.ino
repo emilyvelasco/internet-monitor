65a2db38fca8910cccede1551ce9c28fdc998aa0
emilyvelasco <54129995+emilyvelasco@users.noreply.github.com>
2023-03-18 18:39:47 -0700

    Update internet_monitor.ino

--- internet_monitor.ino
+++ internet_monitor.ino
@@ -1,14 +1,26 @@
+/*This project requires an Arduino Nano 33 IoT.
+Arduino sells other Wi-Fi enabled Arduinos that 
+may work, but I've not tested them with this code.
+
+It was pieced together from some other bits of code, 
+mostly by Aucoeur Ngo, who was helpfully helping me
+troubleshoot why my code wasn't working*/
+
 #include <WiFiNINA.h>
 
+/*wire LEDs to pins 9 and 10 with appropriate resistors.
+This should be about 40-50 ohms for an Arduino with
+3.3V output on its IO pins*/
 #define googleOnlineLED 10
 #define googleOfflineLED 9
 
-
+//your wifi credentials go here
 char ssid[] = "network name";
 char pass[] = "password";
 
 int status = WL_IDLE_STATUS;
 
+//put whichever reliable website you want right here
 char google[] = "google.com";
 
 WiFiClient client;
@@ -19,6 +31,8 @@ void setup() {
   digitalWrite(googleOnlineLED, LOW);
   digitalWrite(googleOfflineLED, LOW);
 
+ /*all the serial comms stuff can be removed, but it's 
+  helpful for troubleshooting*/
   Serial.begin(9600);
 
   while (status != WL_CONNECTED) {
